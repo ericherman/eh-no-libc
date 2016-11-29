65d31f5278294a7c450f2bafd9e8489b5b811f80
Eric Herman <eric@freesa.org>
2016-11-29 20:07:34 +0100

    replace printf() with write()

--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
+
+size_t eh_strlen(char const* str)
+{
+	char const* p;
+	for (p = str; *p; ++p);
+	return p - str;
+}
 
 int main(int argc, char **argv)
 {
-	printf("hello, world\n");
+	const char *str;
+
+	str = "hello, world\n";
+	write(1, str, eh_strlen(str));
 
 	return 0;
 }
