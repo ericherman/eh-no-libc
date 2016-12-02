07caba6a162fbc85be45a28beeb79c7b5e37b94e
Eric Herman <eric@freesa.org>
2016-12-02 22:25:10 +0100

    typedef size_t in terms of uint??_t

--- src/linux-amd64/types.h
+++ src/linux-amd64/types.h
@@ -1,7 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned long size_t;
-typedef long ssize_t;
+typedef unsigned long uint64_t;
+typedef long int64_t;
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
 
 #endif /* TYPES_H */
--- src/linux-i386/types.h
+++ src/linux-i386/types.h
@@ -1,7 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned int size_t;
-typedef int ssize_t;
+typedef unsigned long uint64_t;
+typedef long int64_t;
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+
+typedef uint32_t size_t;
+typedef int32_t ssize_t;
 
 #endif /* TYPES_H */
