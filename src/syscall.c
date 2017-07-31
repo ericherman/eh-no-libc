b6cbd0bd15d0efc3577460728ccb05bdbee705bb
Eric Herman <eric@freesa.org>
2017-07-31 10:05:03 +0200

    add some debug

---
+++ src/syscall.c
@@ -0,0 +1,41 @@
+/*
+eh-no-libc - exploring coding without the standard library
+Copyright (C) 2017 Eric Herman
+
+This work is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later
+version.
+
+This work is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License (COPYING) along with this library; if not, see:
+
+        https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+*/
+#include <syscall.h>
+
+#if ((defined LINUX_I386) || (defined LINUX_AMD64))
+
+static void _eh_segfault(void)
+{
+	*((int *)(NULL)) = -1;
+}
+
+void _eh_crash(const char *msg, size_t len)
+{
+	void *stderr_vpfd = (void *)2;
+
+	syscall3(SYS_write, stderr_vpfd, (void *)"FATAL ERROR '", (void *)13);
+	syscall3(SYS_write, stderr_vpfd, (void *)msg, (void *)len);
+	syscall3(SYS_write, stderr_vpfd, (void *)"', HALT.\n", (void *)9);
+
+	_eh_segfault();
+}
+
+#endif /* if ((defined LINUX_I386) || (defined LINUX_AMD64)) */
