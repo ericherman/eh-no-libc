380981d7a4be1840d4a89c6e17e39eae269fa3ec
Eric Herman <eric@freesa.org>
2017-08-05 14:06:57 +0200

    replace "write(int, char*, size_t)" with "printf(char*, ...)"

--- demo/hello.c
+++ demo/hello.c
@@ -19,19 +19,14 @@ License (COPYING) along with this library; if not, see:
         https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
 */
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	char buf[80];
 	const char *name;
 
 	name = (argc > 1) ? argv[1] : "world";
 
-	snprintf(buf, 80, "hello, %s\n", name);
-
-	write(STDOUT_FILENO, buf, strlen(buf));
+	printf("hello, %s\n", name);
 
 	return 0;
 }
