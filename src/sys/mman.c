4b5a742580e9fd33a4bd94324c4211248ba763b2
Eric Herman <eric@freesa.org>
2017-08-06 15:12:57 +0200

    mmap uses the legacy syscall convention on i386
    Big thanks to Franc[e]sco:
    https://twitter.com/roriicon/status/893115471804989441
    https://github.com/Francesco149/nolibc/blob/052eba8843f11c61e4fdf17f1d11287632fb7cc5/memealloc/main.c#L103
    
    CHEERS!

--- src/sys/mman.c
+++ src/sys/mman.c
@@ -10,6 +10,30 @@
 #include "eh-printf/eh-printf.h"	/* eh_snprintf */
 #endif
 
+#if ((!defined(USE_LEGACY_MMAP)) && defined(LINUX_I386))
+#define USE_LEGACY_MMAP 1
+#endif
+
+#ifndef USE_LEGACY_MMAP
+#define USE_LEGACY_MMAP 0
+#endif
+
+static void *legacy_mmap(void *addr, size_t len, int prot, int flags, int fd,
+			 off_t offset)
+{
+	void *params[7];
+
+	params[0] = addr;
+	params[1] = (void *)((long)len);
+	params[2] = (void *)((long)prot);
+	params[3] = (void *)((long)flags);
+	params[4] = (void *)((long)fd);
+	params[5] = (void *)((long)offset);
+	params[6] = 0;
+
+	return syscall1(SYS_mmap, (void *)params);
+}
+
 void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
 	void *rv;
@@ -18,10 +42,13 @@ void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 	char buf[70];
 #endif
 
-	rv = syscall6(SYS_mmap, addr, (void *)len, (void *)(ssize_t)prot,
-		      (void *)(ssize_t)flags, (void *)(ssize_t)fd,
-		      (void *)(ssize_t)offset);
-
+	if (USE_LEGACY_MMAP) {
+		rv = legacy_mmap(addr, len, prot, flags, fd, offset);
+	} else {
+		rv = syscall6(SYS_mmap, addr, (void *)len,
+			      (void *)(ssize_t)prot, (void *)(ssize_t)flags,
+			      (void *)(ssize_t)fd, (void *)(ssize_t)offset);
+	}
 	s_rv = (long)((ssize_t)rv);
 
 	if (s_rv < 0) {
