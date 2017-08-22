a78f81ede949469675a5332eede24e1b093e4709
Eric Herman <eric@freesa.org>
2017-08-22 14:43:06 +0200

    iso646.h C alternative tokens

---
+++ src/iso646.h
@@ -0,0 +1,36 @@
+/*
+eh-no-libc - exploring coding without the standard library
+Copyright (C) 2017 Eric Herman
+
+This work is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later
+version.
+
+This work is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License (COPYING) along with this library; if not, see:
+
+        https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+*/
+#ifndef _ISO646_H
+#define _ISO646_H	1
+
+#define and	&&
+#define and_eq	&=
+#define bitand	&
+#define bitor	|
+#define compl	~
+#define not	!
+#define not_eq	!=
+#define or	||
+#define or_eq	|=
+#define xor	^
+#define xor_eq	^=
+
+#endif /* _ISO646_H */
