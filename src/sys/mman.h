aed3f29e22c000b15d03582614724b4a59a5c3ec
Eric Herman <eric@freesa.org>
2017-08-23 20:51:19 +0200

    LGPLv2.1+

--- src/sys/mman.h
+++ src/sys/mman.h
@@ -1,3 +1,23 @@
+/*
+eh-no-libc - exploring coding without the standard library
+Copyright (C) 2017 Eric Herman
+
+This work is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later
+version.
+
+This work is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License (COPYING) along with this library; if not, see:
+
+        https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+*/
 #ifndef _SYS_MMAN_H
 #define _SYS_MMAN_H	1
 
