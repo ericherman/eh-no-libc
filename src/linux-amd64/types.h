d2ab19e30f40bc119bee37c32c239964911bd736
Eric Herman <eric@freesa.org>
2017-08-26 13:32:19 +0200

    max_align_t was output in an early "gcc -E", not needed yet.

--- src/linux-amd64/types.h
+++ src/linux-amd64/types.h
@@ -78,11 +78,13 @@ typedef struct {
 	uint64_t rem;
 } imaxdiv_t;
 
+#if 0
 typedef struct {
 	long long __max_align_ll
 	    __attribute__ ((__aligned__(__alignof__(long long))));
 	long double __max_align_ld
 	    __attribute__ ((__aligned__(__alignof__(long double))));
 } max_align_t;
+#endif
 
 #endif /* TYPES_H */
